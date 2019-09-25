843ecc3bd1410e85c66bc321c2b59685b4735049
snopek308 <asnopek486@gmail.com>
2019-09-24 21:51:52 -0500

    updated to connect to database

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class ProductController : Controller
+    {
+        // this controller depends on the NorthwindRepository
+        private INorthwindRepository repository;
+        public ProductController(INorthwindRepository repo) => repository = repo;
+
+        public IActionResult Category() => View(repository.Categories);
+    }
+}
---
+++ Models/INorthwindRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Linq;
+
+namespace Northwind.Models
+{
+    public interface INorthwindRepository
+    {
+        IQueryable<Category> Categories { get; }
+        IQueryable<Product> Products { get; }
+    }
+}
